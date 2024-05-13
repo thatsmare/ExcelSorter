2f8fb9e50e5e4b9da17b7cc0fe07b2c354e4d373
MartynaPen <162487630+thatsmare@users.noreply.github.com>
2024-05-13 23:47:32 +0200

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+//klasa z funkcjami opdowiedzialnymi za caly proces
+class excelsort {
+    public:
+    excelsort();
+void readfiles();
+void match();
+void createfile();
+private:
+std::string tempforcheck;
+std::string datafile1, datafile2;
+std::string dataout;
+
+};
+
+excelsort::excelsort(){
+    std::cout << "Start programu" << std::endl;
+}
+
+ void excelsort::readfiles(){
+
+}
+
+
+int main(){
+
+}
